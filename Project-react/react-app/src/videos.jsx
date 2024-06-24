bec1cbf4534dd3013b9bc7cc68f2db2a0ecd4688
Muaytai <muay-tai@mail.ru>
2024-06-24 17:28:39 +0300

    Рабочий Бэкенд

---
+++ Project-react/react-app/src/videos.jsx
@@ -0,0 +1,45 @@
+import reactLogo from './assets/react.svg'
+
+export const VIDEOS = [
+    {
+        id: 1,
+        title: 'Видео 1',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+    {
+        id: 2,
+        title: 'Видео 2',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+    {
+        id: 3,
+        title: 'Видео 3',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+    {
+        id: 4,
+        title: 'Видео 4',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },    {
+        id: 5,
+        title: 'Видео 5',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+    {
+        id: 6,
+        title: 'Видео 6',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+    {
+        id: 7,
+        title: 'Видео 7',
+        channelName: 'Muay-tai',
+        img: reactLogo,
+    },
+];
