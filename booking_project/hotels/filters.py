839320b6e9fadd0a9743321c8ad8f9a22c0feb6a
Muaytai <muay-tai@mail.ru>
2024-06-28 16:05:19 +0300

    Delete booking_project directory

--- booking_project/hotels/filters.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-
-
-class HotelFilter(filters.OrderingFilter):
-    fields = ['name', 'location', 'rating']
-
